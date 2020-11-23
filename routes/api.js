8102f8cedcc05ce3be3e5f094622318b3efe72f9
Michael Tandy <mike.tandy.1@gmail.com>
2020-11-23 22:03:36 +0000

    removing oauth from mailer

--- routes/api.js
+++ routes/api.js
@@ -28,15 +28,22 @@ app.route('/contact').post((req, res) => {
 
         // NODEMAILER
         var transporter = nodemailer.createTransport({
-          service: "gmail",
+          // service: "gmail",
+          // auth: {
+          //   type: "OAuth2",
+          //   user: process.env.EMAIL,
+          //   clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
+          //   clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+          //   refreshToken: process.env.GOOGLE_OAUTH_REFRESH_TOKEN,
+          //   accessToken: myAccessToken,
+          // },
+          host: 'smtp.gmail.com',
+          port: 465,
+          secure: true,
           auth: {
-            type: "OAuth2",
-            user: process.env.EMAIL,
-            clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-            refreshToken: process.env.GOOGLE_OAUTH_REFRESH_TOKEN,
-            accessToken: myAccessToken,
-          },
+              user: process.env.EMAIL,
+              pass: process.env.EMAIL_PASSWORD
+          }
         });
 
         let mailtoMeMailOptions = {
