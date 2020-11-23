baf886f0e4c73a63909494f812af13e7d3df5eb8
Michael Tandy <mike.tandy.1@gmail.com>
2020-11-23 17:09:18 +0000

    trying different package

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const nodemailer   = require('nodemailer');
 const json              = require('../data/projects.json');
+var nodeoutlook = require('nodejs-nodemailer-outlook');
 
 module.exports = function (app) {
 // get project data
@@ -18,32 +19,44 @@ app.route('/contact').post((req, res) => {
     const myProtonEmail = process.env.PROTON_EMAIL;
     const myPassword = process.env.EMAIL_PASSWORD;
 
-    async function main(){
+    var nodeoutlook = require('nodejs-nodemailer-outlook')
+nodeoutlook.sendEmail({
+    auth: {
+        user: myEmail,
+        pass: myPassword
+    },
+    from: myEmail,
+    to: 'miketandy@protonmail.com',
+    subject: "You've Got Mail",
+    html: `<b>Email: ${email}, Name: ${name}, Message: ${message}</b>`,
+    text: 'This is text version!',})
 
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.office365.com', //"smtp-mail.outlook.com", // hostname
-            secureConnection: false, // TLS requires secureConnection to be false
-            port: 587, // port for secure SMTP
-            requireTLS: true,
-            auth: {
-                user: myEmail,
-                pass: myPassword
-            },
-            tls: {
-                ciphers:'SSLv3'
-            } 
-          });
+    // async function main(){
+
+    //     let transporter = nodemailer.createTransport({
+    //         host: 'smtp.office365.com', //"smtp-mail.outlook.com", // hostname
+    //         secureConnection: false, // TLS requires secureConnection to be false
+    //         port: 587, // port for secure SMTP
+    //         requireTLS: true,
+    //         auth: {
+    //             user: myEmail,
+    //             pass: myPassword
+    //         },
+    //         tls: {
+    //             ciphers:'SSLv3'
+    //         } 
+    //       });
     
-        let info = await transporter.sendMail({
-          from: `"Me" <${myEmail}>`,
-          to: myProtonEmail,
-          subject: "You've Got Mail",
-          text: `name ${name}, email ${email}, message ${message}` 
-        });
+    //     let info = await transporter.sendMail({
+    //       from: `"Me" <${myEmail}>`,
+    //       to: myProtonEmail,
+    //       subject: "You've Got Mail",
+    //       text: `name ${name}, email ${email}, message ${message}` 
+    //     });
     
-        console.log("Email sent");
-      }
-    main().catch(console.error);
+    //     console.log("Email sent");
+    //   }
+    // main().catch(console.error);
 
   res.redirect('/')
 })
