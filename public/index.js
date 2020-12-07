5277fe9f2820fd80a99d56fa7ff36ef55fb4c191
Michael Tandy <mike.tandy.1@gmail.com>
2020-12-07 13:06:12 +0000

    pagination working

--- public/index.js
+++ public/index.js
@@ -1,4 +1,3 @@
-// get project data and display projects
 $(() => {
     const url = '/data/projects';
     $.ajax({
@@ -6,6 +5,7 @@ $(() => {
       url: url,
       success: (data) =>
       {
+
         let projects = [];
         data.projects.forEach((ele, index) => {
             const project = [
@@ -42,11 +42,63 @@ $(() => {
               projects.push(project.join(''));
         });
         $('#projects').html(projects.join(''));
+        showPage(1)
+        
+        //$('.pagination').html(paginationButtons.join(''))
       }
     })
-});
+})
+  pageSize = 5
+
+  showPage = function(page) {
+    $(".grid-item").hide();
+    $(".grid-item").each(function(n) {
+        if (n >= pageSize * (page - 1) && n < pageSize * page)
+            $(this).show();
+    });        
+  }
+
+  // need to keep track of which page the user is currently on for next and prev to work (state)
+
+  let currentPage = 1;
+  showPage(currentPage)
+  
+  $(".pagination li a").click(function(e) {
+    $(".pagination li a").removeClass("current").parent().removeClass('active');
+    if (e.target.id === 'paginate-previous') {
+      console.log($(`#paginate-btn-${currentPage}`))
+      if (currentPage >= 2) { // lower limit
+      currentPage = currentPage-1;
+      }
+      console.log(currentPage)
+      $(`#paginate-btn-${currentPage}`).addClass("current").parent().addClass('active');
+      showPage(currentPage) 
+
+    } else if (e.target.id === 'paginate-next') {
+      console.log($(`#paginate-btn-${currentPage}`))
+      if (currentPage <= 4 ) { // upper limit
+      currentPage++;
+      }
+      console.log(currentPage)
+      $(`#paginate-btn-${currentPage}`).addClass("current").parent().addClass('active');
+      showPage(currentPage) 
+    }  
+    else {
+    $(this).addClass("current").parent().addClass('active');
+    showPage(parseInt($(this).text())) 
+    currentPage = this.innerText;
+    console.log(currentPage)
+    }
+  });
 
 // contact form submit message
 $("#contact-form").submit(() => {
     alert( "Thanks for your message. I will be in touch soon." );
   });
+
+
+$(document).ready(function(){
+    $("#paginate-btn-1").trigger('click'); 
+});
+
+
